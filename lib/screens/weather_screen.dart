69969329ae3b3defe77aacbf75cfb3033633283b
Julian Koripamo <jkoripamo@gmail.com>
2023-11-09 22:17:18 +0100

    weather screen

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
 
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({Key? key}) : super(key: key);
@@ -8,8 +10,71 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
+  // api key
+  final _weatherService = WeatherService('2097d28ec46e82fd3d907106c89fc72b');
+  Weather? _weather;
+
+  // fetch weather
+  _fetchWeather() async {
+    // get the current city
+    String cityName = await _weatherService.getCurrentCity();
+
+    // get weather for the city
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    }
+    // any errors
+    catch (e) {
+      print(e);
+    }
+  }
+
+  // weather animations
+
+  // init state
+  @override
+  void initState() {
+    super.initState();
+
+    // fetch weather on startup
+    _fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey[600],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[900],
+        title: const Row(
+          children: [
+            Icon(Icons.sunny),
+            SizedBox(width: 5),
+            Text('Weather'),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // city name
+            Text(
+              _weather?.cityName ?? "loading city...",
+              style: const TextStyle(color: Colors.white),
+            ),
+
+            // temperature
+            Text(
+              '${_weather?.temperature.round()}°C',
+              style: const TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
